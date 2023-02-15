4e98831a618d0618a64aaf774804c0989a468916
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-15 08:24:53 -0600

    Initial Commit

---
+++ SwiftStateExercise/SwiftStateExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStateExerciseApp.swift
+//  SwiftStateExercise
+//
+//  Created by Ashlyn Barrera on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStateExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
