f27d1a80b04fa4f04ff2bf5b6f3f7202a429266e
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-15 09:15:53 -0600

    finished initial version if exercise and bonus additions.

--- SwiftStateExercise/ContentView.swift
+++ SwiftStateExercise/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+//1a
+    @State var numOne = 0
+    @State var numTwo = 2
+    
+    //Bonus
+    @State var letterIndex = 0
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    
+
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Spacer()
+
+            HStack {
+                Text("ONE")
+                Text(String(numOne))
+                Button("ADD ONE", action: {
+                    numOne = numOne + 1
+                })
+            }
+            
+            
+            Spacer()
+            
+            HStack {
+                Text("TWO")
+                Text(String(numTwo))
+                Button("Squared", action: {
+                    numTwo = numTwo * numTwo
+                } )
+            }
+            
+            Spacer()
+   //Bonus
+            HStack {
+                Text("BONUS")
+                Text(String(letters[letterIndex]))
+                Button( "Next Letter ", action: {
+                    letterIndex = letterIndex + 1
+                    while (letterIndex == 26 ){
+                        letterIndex = 0
+                        
+                    }
+                })
+                
+            }
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
